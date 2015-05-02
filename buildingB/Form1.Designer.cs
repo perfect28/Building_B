b29eef061ce4af8fa7e558570c346e9d9698efd9
perfect28 <532870670@qq.com>
2015-05-02 20:10:11 +0800

    Building_B Navigation

---
+++ buildingB/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace buildingB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.pic_dorm = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_num_south = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pic_south = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pic_north = new System.Windows.Forms.PictureBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txt_num_north = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_dorm)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_south)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_north)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(866, 487);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.pic_dorm);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(858, 461);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "宿舍区导航";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox);
+            this.groupBox1.Location = new System.Drawing.Point(606, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 160);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "选择出发地";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "宿舍区：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "规划路线";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "竹园宿舍区",
+            "海棠宿舍区",
+            "丁香宿舍区"});
+            this.comboBox.Location = new System.Drawing.Point(8, 66);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 20);
+            this.comboBox.TabIndex = 3;
+            // 
+            // pic_dorm
+            // 
+            this.pic_dorm.Image = ((System.Drawing.Image)(resources.GetObject("pic_dorm.Image")));
+            this.pic_dorm.InitialImage = null;
+            this.pic_dorm.Location = new System.Drawing.Point(17, 6);
+            this.pic_dorm.Name = "pic_dorm";
+            this.pic_dorm.Size = new System.Drawing.Size(534, 449);
+            this.pic_dorm.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_dorm.TabIndex = 0;
+            this.pic_dorm.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pic_south);
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(858, 461);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "B楼南导航";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.txt_num_south);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(20, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(516, 70);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "选择目的地";
+            // 
+            // txt_num_south
+            // 
+            this.txt_num_south.Location = new System.Drawing.Point(76, 30);
+            this.txt_num_south.Name = "txt_num_south";
+            this.txt_num_south.Size = new System.Drawing.Size(100, 21);
+            this.txt_num_south.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "房间号";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 24);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "规划路线";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.pic_north);
+            this.tabPage3.Controls.Add(this.groupBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(858, 461);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "B楼北导航";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pic_south
+            // 
+            this.pic_south.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_south.Image = ((System.Drawing.Image)(resources.GetObject("pic_south.Image")));
+            this.pic_south.InitialImage = null;
+            this.pic_south.Location = new System.Drawing.Point(6, 117);
+            this.pic_south.Name = "pic_south";
+            this.pic_south.Size = new System.Drawing.Size(846, 241);
+            this.pic_south.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_south.TabIndex = 7;
+            this.pic_south.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(360, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "清除路线";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pic_north
+            // 
+            this.pic_north.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_north.Image = ((System.Drawing.Image)(resources.GetObject("pic_north.Image")));
+            this.pic_north.InitialImage = null;
+            this.pic_north.Location = new System.Drawing.Point(6, 159);
+            this.pic_north.Name = "pic_north";
+            this.pic_north.Size = new System.Drawing.Size(846, 185);
+            this.pic_north.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic_north.TabIndex = 9;
+            this.pic_north.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.txt_num_north);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Location = new System.Drawing.Point(20, 61);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(516, 70);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "选择目的地";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(360, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "清除路线";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txt_num_north
+            // 
+            this.txt_num_north.Location = new System.Drawing.Point(76, 30);
+            this.txt_num_north.Name = "txt_num_north";
+            this.txt_num_north.Size = new System.Drawing.Size(100, 21);
+            this.txt_num_north.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "房间号";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(205, 27);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 24);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "规划路线";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 503);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_dorm)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_south)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_north)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.PictureBox pic_dorm;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txt_num_south;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pic_south;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pic_north;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txt_num_north;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ buildingB/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buildingB
+{
+    public partial class Form1 : Form
+    {
+
+        const int MAXN = 100;
+        String path = "";
+
+        int cnt_num_south = 0;
+        int[] hash_south = new int[1000];
+        Room[] rooms_south = new Room[MAXN];
+
+        int cnt_num_north = 0;
+        int[] hash_north = new int[1000];
+        Room[] rooms_north = new Room[MAXN];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Init();
+
+            path = Application.StartupPath + "\\south.txt";
+            read_data(path,rooms_south,hash_south,ref cnt_num_south);
+
+            path = Application.StartupPath + "\\north.txt";
+            read_data(path, rooms_north, hash_north, ref cnt_num_north);
+        }
+
+        private void Init()
+        {
+            for(int i=0;i<1000;i++)
+            {
+                hash_north[i] = -1;
+                hash_south[i] = -1;
+            }
+        }
+
+        public struct Point
+        {
+            public int x, y;
+        }
+        public class Room
+        {
+            int root_num;
+
+            public int Num
+            {
+                get { return root_num; }
+                set { root_num = value; }
+            }
+
+            int step_num;
+
+            public int Step_num
+            {
+                get { return step_num; }
+                set { step_num = value; }
+            }
+
+            Point[] path = new Point[MAXN];
+
+            public Point[] Path
+            {
+                get { return path; }
+                set { path = value; }
+            }
+
+            public Room(int num)
+            {
+                this.root_num = num;
+            }
+           
+            public void set_path(int n,Point[] m)
+            {
+                for(int i=1;i<=n;i++)
+                    path[i]=m[i];
+            }
+        }
+
+        private void read_data(String path,Room[] rooms,int[] hash,ref int cnt_num)
+        {    
+            //String path = Application.StartupPath + "\\south.txt";
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //获取房间号
+                int num = int.Parse(line);
+                rooms[++cnt_num] = new Room(num);
+                hash[num] = cnt_num;
+                //获取路径个数
+                line = sr.ReadLine();
+                int n = int.Parse(line);
+                rooms[cnt_num].Step_num = n;
+
+                //获取路径
+                Point[] road = new Point[MAXN];
+                for(int i=1;i<=n;i++)
+                {
+                    line = sr.ReadLine();
+                    string[] messages = line.Split(new char[] {' '});
+                    Point now;
+                    now.x = int.Parse(messages[0]);
+                    now.y = int.Parse(messages[1]);
+                    road[i]=now;
+                }
+                rooms[cnt_num].set_path(n, road);
+            }
+        }
+        
+        int get_dis(int x1,int y1,int x2,int y2)
+        {
+            return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox.SelectedIndex)
+	        {
+                case 0:
+                    this.pic_dorm.Image = Image.FromFile(Application.StartupPath + "\\picture\\south_Bamboo.jpg");
+                    break;
+                case 1:
+                    this.pic_dorm.Image = Image.FromFile(Application.StartupPath + "\\picture\\south_Begonia.jpg"); 
+                    break;
+                case 2:
+                    this.pic_dorm.Image = Image.FromFile(Application.StartupPath + "\\picture\\south_Clove.jpg"); 
+                    break;
+		        default:
+                    break;
+	        }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Graphics g = this.pictureBox1.CreateGraphics();
+            Graphics g = Graphics.FromImage(pic_south.Image);
+            int num = int.Parse(txt_num_south.Text);
+            int id = hash_south[num];
+
+            if (id == -1) 
+            {
+                MessageBox.Show("抱歉，需要寻找的教室不存在~");
+            }
+            else
+            {
+                SolidBrush brush = new SolidBrush(Color.Red);
+                float LineWidth = 5.0f;
+                Pen pen = new Pen(brush, LineWidth);
+
+                for (int i = 1; i < rooms_south[id].Step_num; i++)
+                {
+                    int x1 = rooms_south[id].Path[i].x;
+                    int y1 = rooms_south[id].Path[i].y;
+                    int x2 = rooms_south[id].Path[i + 1].x;
+                    int y2 = rooms_south[id].Path[i + 1].y;
+                    //g.FillEllipse(myBrush, new Rectangle(x, y, 20, 20));
+                    g.DrawLine(pen, x1, y1, x2, y2);//坐标
+                    pic_south.Refresh();
+
+                    int len = get_dis(x1, y1, x2, y2);
+                    Thread.Sleep((int)(len / 100));
+                }
+                g.Dispose();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.pic_south.Image = Image.FromFile(Application.StartupPath + "\\picture\\Building_south_small.png");
+            this.pic_south.Refresh();
+        }
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Graphics g = Graphics.FromImage(pic_north.Image);
+            int num = int.Parse(txt_num_north.Text);
+            int id = hash_north[num];
+
+            if (id == -1)
+            {
+                MessageBox.Show("抱歉，需要寻找的教室不存在~");
+            }
+            else
+            {
+                SolidBrush brush = new SolidBrush(Color.Blue);
+                float LineWidth = 5.0f;
+                Pen pen = new Pen(brush, LineWidth);
+
+                for (int i = 1; i < rooms_north[id].Step_num; i++)
+                {
+                    int x1 = rooms_north[id].Path[i].x;
+                    int y1 = rooms_north[id].Path[i].y;
+                    int x2 = rooms_north[id].Path[i + 1].x;
+                    int y2 = rooms_north[id].Path[i + 1].y;
+                    //g.FillEllipse(myBrush, new Rectangle(x, y, 20, 20));
+
+                    g.DrawLine(pen, x1, y1, x2, y2);//坐标
+                    pic_north.Refresh();
+
+                    int len = get_dis(x1, y1, x2, y2);
+                    Thread.Sleep((int)len / 100);
+                }
+                g.Dispose();
+            }
+        }
+
+        private void paint_line()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.pic_north.Image = Image.FromFile(Application.StartupPath + "\\picture\\Building_north_small.png");
+            this.pic_north.Refresh();
+        }
+    }
+}
